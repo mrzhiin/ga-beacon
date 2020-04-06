6ba62b4f8a0f36b4b51c2212fcd30369329da623
MrZhiin <mrzhiin@gmail.com>
2020-04-06 22:08:22 +0800

    Update

---
+++ src/index.tsx
@@ -0,0 +1,61 @@
+interface PageviewPayload {
+  // 屏幕分辨率
+  sr: string;
+  // 视口大小
+  vp: string;
+  // 文档编码
+  de: string;
+  // 屏幕颜色
+  sd: string;
+  // 用户语言
+  ul: string;
+  // 文档位置网址
+  dl: string;
+  // 文档标题
+  dt: string;
+  // 跟踪 ID
+  tid: string;
+  // 用户代理
+  ua: string;
+  // 文档引荐来源网址
+  dr: string;
+  // 匹配类型
+  t: "pageview";
+}
+
+class GaBeacon {
+  // 跟踪 ID
+  tid: string;
+  // 代理地址
+  proxy: string;
+
+  constructor({ tid = "", proxy = "" }) {
+    this.tid = tid;
+    this.proxy = proxy;
+  }
+
+  pageview() {
+    const payload: PageviewPayload = {
+      tid: this.tid,
+      t: "pageview",
+      sr: `${window.screen.width}x${window.screen.height}`,
+      vp: `${window.innerWidth}x${window.innerHeight}`,
+      de: document.characterSet,
+      sd: `${window.screen.colorDepth}-bits`,
+      ul: `${window.navigator.language}`,
+      dl: `${document.location.origin}${document.location.pathname}${document.location.search}`,
+      dt: document.title,
+      ua: `${navigator.userAgent}`,
+      dr: `${document.referrer}`,
+    };
+
+    this.send(payload);
+  }
+
+  send(data) {
+    navigator.sendBeacon(this.proxy, JSON.stringify(data));
+  }
+}
+
+export default GaBeacon;
+export { GaBeacon };
---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const merge = require("webpack-merge");
+const PnpWebpackPlugin = require(`pnp-webpack-plugin`);
+
+const common = {
+  entry: {
+    main: "./src/index.tsx",
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "gabeacon.js",
+    library: `GaBeacon`,
+    libraryTarget: "umd",
+    libraryExport: "default",
+    globalObject: "this",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        loader: "ts-loader",
+        options: {
+          transpileOnly: true,
+        },
+      },
+    ],
+  },
+  plugins: [new Dotenv(), new webpack.ProgressPlugin()],
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+    plugins: [PnpWebpackPlugin],
+  },
+  resolveLoader: {
+    plugins: [PnpWebpackPlugin.moduleLoader(module)],
+  },
+};
+
+const development = {
+  mode: "development",
+  devtool: "eval-source-map",
+  devServer: {
+    port: 8000,
+    host: "0.0.0.0",
+    hot: true,
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin(), new HtmlWebpackPlugin()],
+};
+
+const production = {
+  mode: "production",
+  plugins: [new CleanWebpackPlugin()],
+};
+
+module.exports = (env = {}) => {
+  return merge(common, env.production ? production : development);
+};
